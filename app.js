a0dce031518efb369f4ebef2f379bfceb231bfe8
pjch8908 <75929653+pjch8908@users.noreply.github.com>
2021-02-13 17:37:31 +0900

    Initial Commit

--- app.js
+++ app.js
@@ -6,19 +6,11 @@ import bodyParser from "body-parser";
 import { userRouter } from "./router";
 const app = express();
 
-const handleHome = (req, res) => res.send("Hello from my ass");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
-
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
-
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
 
 app.use("/user", userRouter);
 
